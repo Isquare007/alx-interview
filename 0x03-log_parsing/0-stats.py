bd357cc9c8a88dd4f65f039cc6ac48ccecdee1fb
Isquare007 <akibrahiim2019@gmail.com>
2023-04-13 13:48:14 +0100

    log parsing checker solved

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -16,12 +16,14 @@ stats = {
 
 total_size = 0
 
+
 def print_stat():
     """prints the infos"""
     print("File size: {}".format(total_size))
     for key, value in sorted(stats.items()):
         if value > 0:
             print("{}: {}".format(key, value))
+
 
 if __name__ == "__main__":
     line_count = 0
@@ -32,7 +34,7 @@ if __name__ == "__main__":
                 status_Code = parts[7]
                 file_size = int(parts[8])
 
-                if status_Code in stats:
+                if status_Code in stats.keys():
                     stats[status_Code] += 1
 
                 total_size += file_size
@@ -42,7 +44,7 @@ if __name__ == "__main__":
 
             if line_count % 10 == 0:
                 print_stat()
-            
+
     except KeyboardInterrupt as e:
         print_stat()
         raise
