91afb0930d1e7c8405e11dafdc70baa74b7d80fe
Isquare007 <akibrahiim2019@gmail.com>
2023-04-19 23:37:09 +0100

    uft

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -34,13 +34,3 @@ def validUTF8(data):
         return True
 
     return False
-
-
-data = [65]
-print(validUTF8(data))
-
-data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-print(validUTF8(data))
-
-data = [229, 65, 127, 256]
-print(validUTF8(data))
