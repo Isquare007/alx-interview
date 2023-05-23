3842aba102133fd246c75a9ec3f5d2c37ab37da7
Isquare007 <akibrahiim2019@gmail.com>
2023-05-23 06:56:26 +0100

    testing

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,84 +1,93 @@
 #!/usr/bin/python3
-"""N queens interview question"""
+"""
+N-Queens Algorithms
+"""
 import sys
 
 
-def print_solutions():
-    """gets n for cli and prints the solution nicely"""
-
-    if len(sys.argv) != 2:
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
     try:
-        n = int(sys.argv[1])
+        n = int(args[1])
     except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+        print('N must be a number')
+        exit(1)
 
     if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+        print('N must be at least 4')
+        exit(1)
 
-    solutions = solve_queen(n)
-    for solution in solutions:
-        queen_pos = []
-        for i, row in enumerate(solution):
-            for j, char in enumerate(row):
-                if char == "Q":
-                    queen_pos.append([i, j])
-        print(queen_pos)
+    row = 0
+    col = 0
+    solutions = []
+    placed_queens = []
+    stop = False
 
+    # iterating through rows and columns
+    while row < n:
+        back_track = False
+        while col < n:
+            # check if current column is safe
+            safe = True
+            for cord in placed_queens:
+                c = cord[1]
+                if (c == col or c + (row-cord[0]) == col or
+                        c - (row-cord[0]) == col):
+                    safe = False
+                    break
 
-def solve_queen(n):
-    """ creates a set for column, updiagonal and downdiagonal of the board,
-    the backtracking make use of them. they contain all the 
-    diagonal, left and right movement of a position
-    Also populates the board with "."
-
-    Args:
-        n (int): number of queens, row and columms
-
-    Returns:
-        list: list of possible solutions
-    """
-    col = set()
-    upDiag = set()  # positive diagonal
-    downDiag = set()  # negative diagonal
-
-    result = []
-    board = [["."] * n for i in range(n)]
-
-    def backtrack(r):
-        """backtracking algorithm that checks for the column, updiagonal
-        and downdiagonal, places the queen there if pos doesn't have
-        a queen that can attack it, else it changes the position of the queen
-
-        Args:
-            r (int): the starting row (0)
-        """
-        if r == n:
-            copy = ["".join(row) for row in board]
-            result.append(copy)
-            return
-        for c in range(n):
-            if c in col or (r + c) in upDiag or (r - c) in downDiag:
+            if not safe:
+                if col == n - 1:
+                    back_track = True
+                    break
+                col += 1
                 continue
-            col.add(c)
-            upDiag.add(r + c)
-            downDiag.add(r - c)
-            board[r][c] = "Q"
 
-            backtrack(r + 1)
+            # place queen
+            cords = [row, col]
+            placed_queens.append(cords)
+            # if it is the last row
+            # append solution and reset all to last unfinished row
+            # and last safe column in that row
+            if row == n - 1:
+                solutions.append(placed_queens[:])
+                for cord in placed_queens:
+                    if cord[1] < n - 1:
+                        row = cord[0]
+                        col = cord[1]
+                for i in range(n - row):
+                    placed_queens.pop()
+                if row == n - 1 and col == n - 1:
+                    placed_queens = []
+                    stop = True
+                row -= 1
+                col += 1
+            else:
+                col = 0
+            break
+        if stop:
+            break
+        # on fail: go back to previous row
+        # and continue from last safe column + 1
+        if back_track:
+            row -= 1
+            while row >= 0:
+                col = placed_queens[row][1] + 1
+                del placed_queens[row]
+                if col < n:
+                    break
+                row -= 1
+            if row < 0:
+                break
+            continue
+        row += 1
 
-            col.remove(c)
-            upDiag.remove(r + c)
-            downDiag.remove(r - c)
-            board[r][c] = "."
-
-    backtrack(0)
-    return result
-
-
-if __name__ == '__main__':
-    print_solutions()
+    for idx, val in enumerate(solutions):
+        if idx == len(solutions) - 1:
+            print(val)
+        else:
+            print(val)
