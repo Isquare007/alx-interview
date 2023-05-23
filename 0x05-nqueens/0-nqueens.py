1f5c1e7e7fd675295612bdeba51052066c4bf813
Isquare007 <akibrahiim2019@gmail.com>
2023-05-23 07:02:06 +0100

    my code

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,134 +1,84 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle.
-Determines all possible solutions to placing N
-N non-attacking queens on an NxN chessboard.
-Example:
-    $ ./0-nqueens.py N
-N must be an integer greater than or equal to 4.
-Attributes:
-    board (list): A list of lists representing the chessboard.
-    solutions (list): A list of lists containing solutions.
-Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
-where `r` and `c` represent the row and column, respectively, where a
-queen must be placed on the chessboard.
-"""
+"""N queens Algorithm (interview question)"""
 import sys
 
 
-def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
-    board = []
-    [board.append([]) for i in range(n)]
-    [row.append(' ') for i in range(n) for row in board]
-    return (board)
+def print_solutions():
+    """gets n for cli and prints the solution nicely"""
 
-
-def board_deepcopy(board):
-    """Return a deepcopy of a chessboard."""
-    if isinstance(board, list):
-        return list(map(board_deepcopy, board))
-    return (board)
-
-
-def get_solution(board):
-    """Return the list of lists representation of a solved chessboard."""
-    solution = []
-    for r in range(len(board)):
-        for c in range(len(board)):
-            if board[r][c] == "Q":
-                solution.append([r, c])
-                break
-    return (solution)
-
-
-def xout(board, row, col):
-    """X out spots on a chessboard.
-    All spots where non-attacking queens can no
-    longer be played are X-ed out.
-    Args:
-        board (list): The current working chessboard.
-        row (int): The row where a queen was last played.
-        col (int): The column where a queen was last played.
-    """
-    # X out all forward spots
-    for c in range(col + 1, len(board)):
-        board[row][c] = "x"
-    # X out all backwards spots
-    for c in range(col - 1, -1, -1):
-        board[row][c] = "x"
-    # X out all spots below
-    for r in range(row + 1, len(board)):
-        board[r][col] = "x"
-    # X out all spots above
-    for r in range(row - 1, -1, -1):
-        board[r][col] = "x"
-    # X out all spots diagonally down to the right
-    c = col + 1
-    for r in range(row + 1, len(board)):
-        if c >= len(board):
-            break
-        board[r][c] = "x"
-        c += 1
-    # X out all spots diagonally up to the left
-    c = col - 1
-    for r in range(row - 1, -1, -1):
-        if c < 0:
-            break
-        board[r][c]
-        c -= 1
-    # X out all spots diagonally up to the right
-    c = col + 1
-    for r in range(row - 1, -1, -1):
-        if c >= len(board):
-            break
-        board[r][c] = "x"
-        c += 1
-    # X out all spots diagonally down to the left
-    c = col - 1
-    for r in range(row + 1, len(board)):
-        if c < 0:
-            break
-        board[r][c] = "x"
-        c -= 1
-
-
-def recursive_solve(board, row, queens, solutions):
-    """Recursively solve an N-queens puzzle.
-    Args:
-        board (list): The current working chessboard.
-        row (int): The current working row.
-        queens (int): The current number of placed queens.
-        solutions (list): A list of lists of solutions.
-    Returns:
-        solutions
-    """
-    if queens == len(board):
-        solutions.append(get_solution(board))
-        return (solutions)
-
-    for c in range(len(board)):
-        if board[row][c] == " ":
-            tmp_board = board_deepcopy(board)
-            tmp_board[row][c] = "Q"
-            xout(tmp_board, row, c)
-            solutions = recursive_solve(tmp_board, row + 1,
-                                        queens + 1, solutions)
-
-    return (solutions)
-
-
-if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if sys.argv[1].isdigit() is False:
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
-    if int(sys.argv[1]) < 4:
+
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    board = init_board(int(sys.argv[1]))
-    solutions = recursive_solve(board, 0, 0, [])
-    for sol in solutions:
-        print(sol)
+    solutions = solve_queen(n)
+    for solution in solutions:
+        queen_pos = []
+        for i, row in enumerate(solution):
+            for j, char in enumerate(row):
+                if char == "Q":
+                    queen_pos.append([i, j])
+        print(queen_pos)
+
+
+def solve_queen(n):
+    """ creates a set for column, updiagonal and downdiagonal of the board,
+    the backtracking make use of them. they contain all the 
+    diagonal, left and right movement of a position
+    Also populates the board with "."
+
+    Args:
+        n (int): number of queens, row and columms
+
+    Returns:
+        list: list of possible solutions
+    """
+    col = set()
+    upDiag = set()  # positive diagonal
+    downDiag = set()  # negative diagonal
+
+    result = []
+    board = [["."] * n for i in range(n)]
+
+    def backtrack(r):
+        """backtracking algorithm that checks for the column, updiagonal
+        and downdiagonal, places the queen there if pos doesn't have
+        a queen that can attack it, else it changes the position of the queen
+
+        Args:
+            r (int): the starting row (0)
+        """
+        if r == n:
+            copy = ["".join(row) for row in board]
+            result.append(copy)
+            return
+        for c in range(n):
+            if c in col or (r + c) in upDiag or (r - c) in downDiag:
+                continue
+            col.add(c)
+            upDiag.add(r + c)
+            downDiag.add(r - c)
+            board[r][c] = "Q"
+
+            backtrack(r + 1)
+
+            col.remove(c)
+            upDiag.remove(r + c)
+            downDiag.remove(r - c)
+            board[r][c] = "."
+
+    backtrack(0)
+    return result
+
+
+if __name__ == '__main__':
+    print_solutions()
