90924b25c014cc336a72f88fb116ef7db145eebc
Isquare007 <akibrahiim2019@gmail.com>
2023-06-07 15:23:54 +0100

    2d rotator

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""2D matrix rotator"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[list]) -> None:
+    """rotates a 2d matrix"""
+    m = len(matrix)
+    for i in range(m):
+        for y in range(i, m):
+            matrix[i][y], matrix[y][i] = matrix[y][i], matrix[i][y]
+
+    for i in range(m):
+        matrix[i] = matrix[i][::-1]
