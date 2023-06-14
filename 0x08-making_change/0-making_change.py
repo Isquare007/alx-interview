008ae94d0a2bb787e1eafe10c95db618b640b531
Isquare007 <akibrahiim2019@gmail.com>
2023-06-14 18:41:23 +0100

    alx is picking efficiency over accuracy sigh

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,29 +7,28 @@ def makeChange(coins, total):
     fewest number of coins needed to meet a given amount total.
     """
     coins.sort(reverse=True)
-    
+
     # Initialize a counter for the number of coins needed
     count = 0
-    
+
     # Iterate through each coin denomination
     for coin in coins:
         # Count the number of coins needed for the current denomination
         coin_count = total // coin
         count += coin_count
-        
+
         # Update the total
         total -= coin * coin_count
-        
+
         # Check if the total has been reduced to 0
         if total == 0:
             break
-    
+
     # If the total is not reduced to 0, it cannot be made with the given coins
     if total != 0:
         return -1  # Or any other appropriate value to indicate impossibility
-    
-    return count
 
+    return count
 
 
 # -----------------------_________---------------____________----------------_________----------
@@ -40,7 +39,7 @@ def makeChange(coins, total):
 #     """
 #     if total <= 0:
 #         return 0
-    
+
 #     # sets dp to a list length of total+1 each number is total+1
 #     dp = [total + 1] * (total + 1)
 #     # base case sets dp[0] to 0
