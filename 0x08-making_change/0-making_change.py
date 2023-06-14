1d22ffcc2bee439a9ac9fc58f211d7698c98e24e
Isquare007 <akibrahiim2019@gmail.com>
2023-06-14 17:46:47 +0100

    making the if conditon more efficient

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,7 +14,7 @@ def makeChange(coins, total):
 
     for t in range(1, total + 1):
         for c in coins:
-            if t - c >= 0:
+            if c <= t:
                 dp[t] = min(dp[t], 1 + dp[t - c])
 
     return dp[total] if dp[total] != total + 1 else -1
