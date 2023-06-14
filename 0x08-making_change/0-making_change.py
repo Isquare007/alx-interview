584221022ca366ff2b8725a12123682eed5e0370
Isquare007 <akibrahiim2019@gmail.com>
2023-06-14 16:34:02 +0100

    makechange solution using dynamic programming

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""making_change"""
+
+
+def makeChange(coins, total):
+    """Given a pile of coins of different values, determine the
+    fewest number of coins needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
+
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for t in range(1, total + 1):
+        for c in coins:
+            if t - c >= 0:
+                dp[t] = min(dp[t], 1 + dp[t - c])
+
+    return dp[total] if dp[total] != total + 1 else -1
